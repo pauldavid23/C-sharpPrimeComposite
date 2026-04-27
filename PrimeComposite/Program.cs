d041ab80749227f41e4511c62613a60952ee7919
pauldavid23 <paul.david.gelario@gmail.com>
2026-04-27 23:57:16 +0800

    Add project files.

---
+++ PrimeComposite/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 2)
+{
+    Console.WriteLine("Number must be greater than 1.");
+    Console.ReadKey();
+}
+else if (IsPrime(number))
+{
+    Console.WriteLine($"{number} is a prime number.");
+    Console.ReadKey();
+}
+else
+{
+    Console.WriteLine($"{number} is a composite number.");
+    Console.ReadKey();
+}
+
+bool IsPrime(int n)
+{
+    if (n < 2) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+
+    for (int i = 3; i * i <= n; i += 2)
+    {
+        if (n % i == 0) return false;
+    }
+
+    return true;
+}
